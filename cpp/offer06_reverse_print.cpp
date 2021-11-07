5ce420b01dd960ff0f390f64df8600007110ba7a
zhaoqing <zhaoqing@kuaishou.com>
2021-11-07 18:45:56 +0800

    update new exam

--- cpp/offer06_reverse_print.cpp
+++ cpp/offer06_reverse_print.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <stack>
 using namespace std;
 struct ListNode {
     int val;
@@ -20,6 +21,19 @@ class Solution {
             getNum(head);
             return reversed;
         }
+        vector<int> reverseList_stack(ListNode* head) {
+            vector<int> reversed;
+            stack<int> aux_stack;
+            while(head != NULL) {
+                aux_stack.push(head->val);
+                head = head->next;
+            }
+            while(!aux_stack.empty()) {
+                reversed.push_back(aux_stack.top());
+                aux_stack.pop();
+            }
+            return reversed;
+        }
 
 };
 
@@ -31,7 +45,7 @@ int main() {
     head->next->next->next = new ListNode(4);
 
     Solution* s = new Solution();
-    vector<int> list = s->reverseList(head);
+    vector<int> list = s->reverseList_stack(head);
 
     for(unsigned long i=0; i<list.size(); i++) {
         cout<<list[i]<<" ";
